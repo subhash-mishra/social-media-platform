50edfee4fa8bc35c540f1e8a4a2baa9c5c769c57
subhash-mishra <66115182+subhash-mishra@users.noreply.github.com>
2020-08-26 19:49:22 +0530

    Add files via upload
    connect folder

---
+++ admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+admin.site.register(UserDataBase)
+admin.site.register(Connections)
+admin.site.register(Company_Model)
